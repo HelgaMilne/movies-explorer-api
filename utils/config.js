74743403a153e245b4953f37aeec211910b8cd7a
Olga Milne <kiko-riko@yandex.ru>
2023-07-24 09:55:44 +0300

    fix config

--- utils/config.js
+++ utils/config.js
@@ -8,8 +8,8 @@ const DEV_PORT = 3000;
 const DEV_PATH_DB = 'mongodb://127.0.0.1:27017/testdb';
 const DEV_JWT_SECRET_KEY = 'some-secret-key';
 
-const DB = NODE_ENV === 'production' && PORT ? PORT : DEV_PORT;
-const SERVER_PORT = NODE_ENV === 'production' && PATH_DB ? PATH_DB : DEV_PATH_DB;
+const DB = NODE_ENV === 'production' && PATH_DB ? PATH_DB : DEV_PATH_DB;
+const SERVER_PORT = NODE_ENV === 'production' && PORT ? PORT : DEV_PORT;
 const SECRET_KEY = NODE_ENV === 'production' && JWT_SECRET_KEY ? JWT_SECRET_KEY : DEV_JWT_SECRET_KEY;
 const SECURE_STATUS = NODE_ENV === 'production';
 
